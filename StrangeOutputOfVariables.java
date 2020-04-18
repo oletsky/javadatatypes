b25b2284df6bdd236cbecb6b8473ea540c8eb1e7
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-18 08:59:07 +0300

    Add files via upload

---
+++ StrangeOutputOfVariables.java
@@ -0,0 +1,17 @@
+package javatraining.oletsky.datatypes;
+
+/**
+ * @author O.Oletsky
+ * A programmer intended to output the values of two variables separated
+ * by space.
+ * But something went wrong.
+ * Try to find an error
+ */
+
+public class StrangeOutputOfVariables {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        System.out.println(a+' '+b);
+    }
+}
