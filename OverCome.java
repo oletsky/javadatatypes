fb042e1b9d1640c6119a378d9d6ef25c975c22cf
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-18 08:28:22 +0300

    Add files via upload

---
+++ OverCome.java
@@ -0,0 +1,15 @@
+package javatraining.oletsky.datatypes;
+
+/**
+ * @author Oletsky Oleksiy
+ * The output of this code is false
+ * Find the value of x so that the output would be true
+ */
+
+public class OverCome {
+
+    public static void main(String[] args) {
+        long x=10;
+        System.out.println(x++>x);
+    }
+}
