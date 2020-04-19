0dd73eb5c10d5d269e7984de3b4297825c2b0b84
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-19 16:02:03 +0300

    Add files via upload

---
+++ TypeInterpreting.java
@@ -0,0 +1,15 @@
+package javatraining.oletsky.datatypes;
+
+public class TypeInterpreting {
+    public static void main(String[] args) {
+        //Int and char
+        int a=65;
+        System.out.println(a); //65
+        System.out.println((char) a); //A has such code
+
+        //Int and byte
+        int b =253;
+        System.out.println(b); //253
+        System.out.println((byte) b); //-3 (additional code}
+    }
+}
