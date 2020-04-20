02ce55a1b6ab6ada6fc984e01a1578fe32dda0af
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-20 14:02:03 +0300

    Add files via upload

---
+++ DemoShifts.java
@@ -0,0 +1,15 @@
+package javatraining.oletsky.datatypes;
+
+/**
+ * Illustrating shifts
+ */
+public class DemoShifts {
+    public static void main(String[] args) {
+        int a = 6;
+        System.out.println(a>>1); //3
+        System.out.println(a<<1); //12
+        System.out.println(a<<5); //192
+        System.out.println(a>>2); //1
+    }
+
+}
