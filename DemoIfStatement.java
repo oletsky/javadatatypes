165b1db969f2f273437baec36f46a66301ed29dd
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-20 14:06:44 +0300

    Add files via upload

---
+++ DemoIfStatement.java
@@ -0,0 +1,14 @@
+package javatraining.oletsky.statements;
+
+/**
+ * Illustrating if
+ */
+public class DemoIfStatement {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if (a>b) System.out.println("Max is "+a);
+        else System.out.println("Max is "+b);
+
+    }
+}
