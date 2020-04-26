33f2c04df49ffcbd7a7706c26611ddb8d2dbe909
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 11:01:25 +0300

    Add files via upload

--- DemoBoolean.java
+++ DemoBoolean.java
@@ -1,5 +1,8 @@
 package javatraining.oletsky.datatypes;
 
+/**
+ * Demonstrates possible usage of boolean type
+ */
 public class DemoBoolean {
     public static void main(String[] args) {
         boolean b=false;
