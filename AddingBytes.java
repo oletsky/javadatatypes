7ab9105e401901675cf26e470bcdbe22cac3f7a8
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 14:48:25 +0300

    Add files via upload

---
+++ AddingBytes.java
@@ -0,0 +1,17 @@
+package javatraining.oletsky.datatypes;
+
+/**
+ * @author O.Oletsky
+ * A programmer intended to add two values.
+ * But something went wrong.
+ * Try to fix the mistake
+ */
+
+public class AddingBytes {
+    public static void main(String[] args) {
+        byte b1=5;
+        byte b2=1;
+        byte b=b1+b2;
+        System.out.println(b);
+    }
+}
