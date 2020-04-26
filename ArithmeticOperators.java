f5053a9e1b652616d312e17947de54a50564b134
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 17:32:56 +0300

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,13 @@
+package javatraining.oletsky.datatypes;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        System.out.println(2.+2); //4
+        System.out.println(5-9); //-4
+        System.out.println(2.*8.); //16
+        System.out.println(7/2);  //3
+        System.out.println(7./2); //3.5
+        System.out.println(7%2);  //1
+
+    }
+}
