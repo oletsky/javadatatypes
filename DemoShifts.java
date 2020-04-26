0e3717bbca644b146c961f5fd1d187045b1c8b41
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 21:10:32 +0300

    Add files via upload

--- DemoShifts.java
+++ DemoShifts.java
@@ -10,6 +10,13 @@ public class DemoShifts {
         System.out.println(a<<1); //12
         System.out.println(a<<5); //192
         System.out.println(a>>2); //1
+        System.out.println("-----------");
+        int q = -6;
+        System.out.println(q >> 1); // -3
+        System.out.println(q << 1); // -12
+        System.out.println(q >> 2); // -2
+        System.out.println(q >>> 1); //2147483645
+
     }
 
 }
