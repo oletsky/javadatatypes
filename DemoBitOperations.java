76e0f72b4269dfb11c6f8be6f0641e841e1dba95
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 19:48:24 +0300

    Update DemoBitOperations.java

--- DemoBitOperations.java
+++ DemoBitOperations.java
@@ -7,8 +7,8 @@ package javatraining.oletsky.datatypes;
  */
 public class DemoBitOperations {
     public static void main(String[] args) {
-        byte a = 0b0101010;
-        byte b = 0b1010110;
+        byte a = 0b0101010; //42
+        byte b = 0b1010110; //86
         System.out.println("a="+a+"; b="+b);
         System.out.println(~a);
         System.out.println(a|b);
