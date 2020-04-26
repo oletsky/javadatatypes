3874dd7ac79d129d10f05fc380e82f2d440ee6ef
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 19:08:32 +0300

    Add files via upload

---
+++ DemoLogicalOperators.java
@@ -0,0 +1,13 @@
+package javatraining.oletsky.datatypes;
+
+public class DemoLogicalOperators {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(!a);
+        System.out.println(a^b);
+
+    }
+}
