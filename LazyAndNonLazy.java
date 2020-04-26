cb98b3391d7f6a55203c4011c0f15877d80c076b
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 19:20:46 +0300

    Add files via upload

---
+++ LazyAndNonLazy.java
@@ -0,0 +1,9 @@
+package javatraining.oletsky.datatypes;
+
+public class LazyAndNonLazy {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = false;
+        System.out.println(a&&b);
+    }
+}
