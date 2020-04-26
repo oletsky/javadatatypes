739288132e8a1d32feb301ee869cab3717c5988e
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 20:32:08 +0300

    Add files via upload

---
+++ SimpleUnsignedShift.java
@@ -0,0 +1,9 @@
+package javatraining.oletsky.datatypes;
+
+public class SimpleUnsignedShift {
+    public static void main(String[] args) {
+        byte b = 0b00001100; //12
+        System.out.println(b);
+        System.out.println(b>>1); //6
+    }
+}
