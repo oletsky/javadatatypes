7e237772f88fd7596415523ef7b17ebcdac05972
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 21:02:52 +0300

    Add files via upload

---
+++ SimpleSignedShift.java
@@ -0,0 +1,9 @@
+package javatraining.oletsky.datatypes;
+
+public class SimpleSignedShift {
+    public static void main(String[] args) {
+        byte b = (byte) 0b11111000;
+        System.out.println(b);
+        System.out.println(b>>1);
+    }
+}
