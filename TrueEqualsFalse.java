5ea557197c1b4047f0f34574b729670204b80361
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 17:09:03 +0300

    Add files via upload

---
+++ TrueEqualsFalse.java
@@ -0,0 +1,10 @@
+package javatraining.oletsky.datatypes;
+
+public class TrueEqualsFalse {
+    public static void main(String[] args) {
+        boolean b = false;
+        if (b=true) System.out.println("True");
+        else System.out.println("False");
+
+    }
+}
