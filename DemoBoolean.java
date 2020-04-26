1ca2e1153665fb0df9de049bdb14b67a43037fb2
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 10:53:24 +0300

    Add files via upload

---
+++ DemoBoolean.java
@@ -0,0 +1,9 @@
+package javatraining.oletsky.datatypes;
+
+public class DemoBoolean {
+    public static void main(String[] args) {
+        boolean b=false;
+        if (b) System.out.println("Right");
+        else System.out.println("Wrong");
+    }
+}
