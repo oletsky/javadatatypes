c9ac3695d738853678ab2d9bae374e5ddb63874f
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-26 11:41:16 +0300

    Add files via upload

---
+++ OverFlowTester.java
@@ -0,0 +1,10 @@
+package javatraining.oletsky.datatypes;
+
+public class OverFlowTester {
+    public static void main(String[] args) {
+        int a=1234567897;
+        int b=987654321;
+        int c=a*b;
+        System.out.println(c);
+    }
+}
