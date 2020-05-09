deeeb3315b64fe50d660fa02a4168fb0cc599bec
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-09 17:57:54 +0300

    Add files via upload

---
+++ CharacterLoop.java
@@ -0,0 +1,12 @@
+package javatraining.oletsky.statements;
+
+/**
+ * An illustration of a very simple loop by characters
+ */
+public class CharacterLoop {
+    public static void main(String[] args) {
+        for (char c='a'; c<='z'; c++) {
+            System.out.print(c+" ");
+        }
+    }
+}
